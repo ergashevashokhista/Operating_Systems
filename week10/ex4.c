a7ca915bc94d8a967d6c8c66cf836ad489ea3f29
Sherin <45159450+ShokhistaErgasheva@users.noreply.github.com>
2019-10-23 13:01:30 +0300

    Update ex4.c

--- week10/ex4.c
+++ week10/ex4.c
@@ -1,35 +1,35 @@
 #include <unistd.h>
 #include <stdio.h>
-#include <dirent.h>
+#include <dirent.h>     //To access a directory, use the opendir() function. It’s prototyped in the dirent.h header file
 #include <string.h>
 #include <sys/stat.h>
 #include <stdlib.h>
 
-void searchDir(char *dir, int depth) {
-    DIR *dp;
-    struct dirent *entry;
+void searchDir(char *dir, int depth) {      //The function requires a string argument, a name or path to a directory. 
+    DIR *dp;                                // The function requires a DIR pointer
+    struct dirent *entry;                   //
     struct stat stat;
-    if ((dp = opendir(dir)) == NULL) {
+    if ((dp = opendir(dir)) == NULL) {      //The DIR pointer variable folder acts as the directory handle for the opendir() function 
         fprintf(stderr, "cannot open directory: %s\n", dir);
         return;
     }
-    chdir(dir);
-    while ((entry = readdir(dp)) != NULL) {
-        lstat(entry->d_name, &stat);
-        if (S_ISDIR(stat.st_mode)) {
-            if (strcmp(".", entry->d_name) == 0 ||
+    chdir(dir);                                         // change the current directory
+    while ((entry = readdir(dp)) != NULL) {  
+        lstat(entry->d_name, &stat);                 //lstat() function gets status information about a specified file 
+        if (S_ISDIR(stat.st_mode)) {                    // We check file type with st_mode field (S_ISDIR - directory?)
+            if (strcmp(".", entry->d_name) == 0 ||          // On success, zero is returned. On error, -1 is returned
                 strcmp("..", entry->d_name) == 0)
                 continue;
             printf("%*s%s/\n", depth, "", entry->d_name);
             searchDir(entry->d_name, depth + 4);
         } else {
-            if (stat.st_nlink >= 2) {
-                printf("%*s%s\n", depth, "", entry->d_name);
+            if (stat.st_nlink >= 2) {                           //checks whether hard link count is two or more
+                printf("%*s%s\n", depth, "", entry->d_name);        //print all file names that point to the file
             }
         }
     }
     chdir("..");
-    closedir(dp);
+    closedir(dp);       //close the directory
 }
 
 int main() {
