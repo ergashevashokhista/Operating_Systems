0f5dea7a3fe876130b5169f6bfc0070e69da0e8a
Sherin <45159450+ergashevashokhista@users.noreply.github.com>
2020-09-01 16:08:37 +0300

    Update ex2.c

--- week3/ex2.c
+++ week3/ex2.c
@@ -1,46 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ARR_SIZE(20);
+void bubble_sort (int arr[], int arr_size);
+void print (int arr[], int arr_size);
 
-void buble_sort(int arr[], int arr_len);
-void swap(int *a, int *b);
-void print_array(int arr[], int arr_size);
-
-int main(void){
-	int arr[]={3, 0, 1, 8, 7, 2, 5, 4, 6, 9};
-	buble_sort(arr, ARR_SIZE(arr));
-	print_array(arr, ARR_SIZE(arr));
-	return EXIT_SUCCESS;
+int main()
+{
+    //int arr[5] = {7,8,2,4,9};
+    int n, i;
+    printf("\n Enter the size of the array: \n");
+    scanf("%d",&n);
+    int arr_size=n;
+    int arr[n];
+    printf("\n Enter the array elements: \n");
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    bubble_sort(arr, arr_size);
+    print(arr, arr_size);
+    return 0;
 }
+void bubble_sort(int arr[], int arr_size){
+    int i;
+    int j;
+    for( i=0; i<arr_size; i++){
+        for(j = 0; j<arr_size-1; j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
 
-void buble_sort(int arr[], int arr_len){ 
-	int i, j;
-
-	for (int i = 0; i < arr_len; j++)
-	{
-		for (int j = 0; j < arr_len; j++)
-		{
-			if (arr[j]>arr[j+1])
-			{
-				swap(&arr[j], &arr[j+1]);
-			}
-		}
-	}
 }
+void print(int arr[], int arr_size){
+    int i;
+    for(i = 0; i<arr_size; i++){
+        printf("%d ", arr[i]);
+    }
 
-void swap(int *a, int *b){
-	int temp;
-	temp=*a;
-	*a=*b;
-	*b=temp;
-}
-
-void print_array(int arr[], int arr_size){
-	int i=0;
-
-	for (int i = 0; i < arr_size; i++)
-	{
-		printf("%d\n", arr[i]);
-	}
 }
